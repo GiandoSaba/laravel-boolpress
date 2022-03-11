8eb8e9a6ee4a9d6a8a170eab1bf84a9dbc1d8ca3
GiandoSaba <giandosaba997@gmail.com>
2022-03-11 15:50:17 +0100

    better contacts result

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -10,10 +10,19 @@ class ContactController extends Controller
     public function sendMessage(Request $request)
     {
         $path = $request->file->store('uploads');
+        $firstname = $request->firstname;
+        $lastname = $request->lastname;
+        $message = $request->message;
 
         return response()->json([
             'success' => true,
-            'result' => $path,
+            'result' =>
+            [
+                'firstname' => $firstname,
+                'lastname' => $lastname,
+                'message' => $message,
+                'file_path' => $path,
+            ]
         ]);
     }
 }
